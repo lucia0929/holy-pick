0390603cb237c31843af122447c0966da08ff8d8
lucia0929 <jsu02062@daum.net>
2023-12-25 21:42:27 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import words from './words';
+import './App.css'
 
 function App() {
+  const [name, setName] = useState('');
+  const [randomWord, setRandomWord] = useState({});
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const pickRandomWord = () => {
+    const randomIndex = Math.floor(Math.random() * words.length);
+    setRandomWord(words[randomIndex]);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>가톨릭 말씀 사탕 뽑기</h1>
       </header>
+      <div className="App-content">
+        <input
+          type="text"
+          value={name}
+          onChange={handleNameChange}
+          placeholder="세례명을 입력하세요"
+          className="Name-input"
+        />
+        <button onClick={pickRandomWord} className="Pick-button">말씀 뽑기</button>
+        {randomWord.content && (
+          <div className="Word-display">
+            <p className="Word-content">{randomWord.content}</p>
+            <p className="Word-reference">({randomWord.book} {randomWord.chapter}:{randomWord.verse})</p>
+          </div>
+        )}
+      </div>
+      <footer className="App-footer"></footer>
     </div>
   );
 }
